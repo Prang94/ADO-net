86a44a224abc7250f6ec9484fcb016eae118e2cf
Prang94 <143353900+Prang94@users.noreply.github.com>
2024-03-29 01:28:20 +0300

    Add files via upload

---
+++ ConsoleApp5/Constans.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using ConsoleApp5;
+
+namespace ConsoleApp5
+{
+    internal class Constans
+    {
+        public const string DatabaseFile = "Invest.sqlite";
+        public const string DatabaseConnectionString = "Data Source=" + DatabaseFile;
+
+        public string SetTable()
+        {
+            SqliteConnection sqliteConnection = new SqliteConnection(DatabaseConnectionString);
+            using var command = sqliteConnection.CreateCommand();
+            command.CommandType = System.Data.CommandType.Text;
+            Console.WriteLine("Какую таблицу хотите заполнить?");
+            string nameTable = Console.ReadLine();
+            Console.WriteLine($"name {nameTable}:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"number {nameTable}:");
+            int num = int.Parse( Console.ReadLine() );
+            Console.WriteLine($"date {nameTable} (xx-xx-xxxx):");
+            string dates = Console.ReadLine();
+
+            return $"INSERT INTO {nameTable}(Name, Number, Data) VALUES ('{nameTable}', {num}, '{dates}')";
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp5;
+using Microsoft.Data.Sqlite;
+
+Constans C = new Constans();
+
+File.Create(path:Constans.DatabaseFile).Close();
+
+using (SqliteConnection databaseConnection = new SqliteConnection(Constans.DatabaseConnectionString))
+{
+    databaseConnection.Open();
+
+    using (var command = databaseConnection.CreateCommand())
+    {
+        command.CommandType = System.Data.CommandType.Text;
+        command.CommandText = "CREATE TABLE Brent (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Name Text(50), Number INT, Data DATE);" +
+                              "CREATE TABLE Gold (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Name Text(50), Number INT, Data DATE);" +
+                              "CREATE TABLE RTS (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Name Text(50), Number INT, Data DATE);";
+
+        command.ExecuteNonQuery();
+
+        command.CommandText = C.SetTable();
+        command.ExecuteNonQuery();
+
+        command.CommandText = C.SetTable();
+        command.ExecuteNonQuery();
+
+        command.CommandText = C.SetTable();
+        command.ExecuteNonQuery();
+
+    }
+
+    databaseConnection.Close();
+}
